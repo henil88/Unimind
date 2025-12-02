d8f0bad578328365dbb4c6a096c44d589661731e
henil88 <henilrathod88@gmail.com>
2025-12-02 12:17:54 +0530

    debugg

--- backend/src/middleware/auth.middlware.js
+++ backend/src/middleware/auth.middlware.js
@@ -3,7 +3,7 @@ const userModel = require("../models/user.model");
 
 async function authUser(req, res, next) {
   const { token } = req.cookies;
-
+  console.log(req.cookies);
   if (!token) {
     return res.status(401).json({
       message: "unauthorized login first",
