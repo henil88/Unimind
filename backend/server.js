5c674da793b3b08957f9ecc19fd6caf33d6f44b5
henil88 <henilrathod88@gmail.com>
2025-12-02 10:35:47 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const initialSocketServer = require("./src/socket/socket.server");
 const app = buildApp();
 const httpServer = createServer(app);
 initialSocketServer(httpServer);
-const port = 3000;
+const port = process.env.PORT || 3000;
 connectDb();
 httpServer.listen(port, () => {
   console.log(`server is running on port ${port}`);
