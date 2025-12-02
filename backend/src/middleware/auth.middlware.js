3567208436f1ecd39545b2e2dbd568da2688bfec
henil88 <henilrathod88@gmail.com>
2025-12-02 12:32:53 +0530

    loogg debuuge

--- backend/src/middleware/auth.middlware.js
+++ backend/src/middleware/auth.middlware.js
@@ -3,6 +3,7 @@ const userModel = require("../models/user.model");
 
 async function authUser(req, res, next) {
   const { token } = req.cookies;
+  console.log(token);
   console.log(req.cookies);
   if (!token) {
     return res.status(401).json({
